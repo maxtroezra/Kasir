55ecf0ed8809cb2ba68a97af41bac04f36c078f1
unknown <maestroezra04@gmail.com>
2024-04-12 17:15:35 +0700

    sec commit

---
+++ src/components/Login.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container-xl bg-black">100% wide until small breakpoint</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import router from './router'
+import 'bootstrap';
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
